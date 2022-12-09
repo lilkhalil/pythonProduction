a1577ea57d928b872d2dab2d3fd964e9ec306fe5
aidar <aidarkhalilov4021@mail.ru>
2022-12-09 21:25:41 +0300

    Styles added

--- http-client/src/index.php
+++ http-client/src/index.php
@@ -2,16 +2,53 @@
 <head>
     <title>HTML Service</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        body {
+            font-family: "Jost", sans-serif;
+            color: white;
+            background-image: url('https://wallpaper.dog/large/445126.jpg');
+        }
+        .container {
+            border: 4px solid white;
+            border-radius: 15px;
+            background-color: rgb(26, 26, 26);
+            margin-top: 15px;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        .list-group .list-group-item {
+            margin-top: 5px;
+            border: 1px solid black;
+            border-radius: 15px;
+            color: black;
+        }
+        .form-control.form-control-lg {
+            border: 2px solid black;
+            border-radius: 15px;
+        }
+        .btn.btn-info {
+            border: 2px solid white;
+            border-radius: 15px;
+        }
+    </style>
 </head>
 <body>
-    <h2>App Page</h2>
-    <h3>HTML Text Selector</h3>
-    <form action="index.php" method="post">
-        <input id="url" name="url" class="form-control" placeholder="Type URL...">
-        <input type="submit" class="btn btn-success" value="Execute">
-    </form>
+    <div class="container form">
+        <div class="container-fluid">
+            <h2>Simple Web Application</h2>
+        </div>
+        <h2>Find out your text fragments</h2>
+        <form action="index.php" method="post">
+            <div class="form-group">
+                <input id="url" name="url" class="form-control form-control-lg" placeholder="Type URL...">
+            </div>
+            <button type="submit" class="btn btn-info">Submit</button>
+        </form>
+    </div>
+    <br><br>
     <?php
     if (isset($_POST['url'])) {
         $myCurl = curl_init();
@@ -26,12 +63,12 @@
         $json = json_decode($response);
         $url = $json->url;
         $array = $json->text;
-        echo "<h3>URL: " . $url . "</h3>";
-        echo "<ul>";
+        echo "<div class=\"container response\"><div class=\"container-fluid\"><h2>Найденные фрагменты</h2></div>";
+        echo "<ul class=\"list-group\">";
         foreach($array as $value) {
-            echo "<li>" . $value . "</li>";
+            echo "<li class=\"list-group-item\">" . $value . "</li>";
         }
-        echo "</ul>";
+        echo "</ul></div>";
     }
     ?>
 </body>
